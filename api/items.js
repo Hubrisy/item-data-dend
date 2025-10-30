0f77e993592bd847715799ccce037dbc72d5f29d
Vladimir <79149193+Hubrisy@users.noreply.github.com>
2025-10-30 17:14:54 +0200

    Update items.js

--- api/items.js
+++ api/items.js
@@ -1,1 +1,12 @@
 
+export default async function handler(req, res) {
+  const { file = "items.json" } = req.query;
+  const fs = await import("fs/promises");
+
+  try {
+    const data = await fs.readFile(`./${file}`, "utf-8");
+    res.status(200).json(JSON.parse(data));
+  } catch (err) {
+    res.status(404).json({ error: "File not found" });
+  }
+}
